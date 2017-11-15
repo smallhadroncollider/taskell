db7a0a878565a15d997e59be02e30f130ef47a00
Mark Wales <mark@smallhadroncollider.com>
2017-11-15 16:32:42 +0000

    added vty

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Lib (render)
 
 main :: IO ()
-main = someFunc
+main = render
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,23 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (
+    render
+) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Graphics.Vty
+
+draw vty = do
+     let title = string (defAttr ` withForeColor ` green) ("Taskell")
+         img = title
+         pic = picForImage img
+     update vty pic
+
+     e <- nextEvent vty
+
+     if (show e) == "EvKey (KChar 'q') []"
+         then shutdown vty
+         else draw vty
+
+render :: IO ()
+render = do
+     cfg <- standardIOConfig
+     vty <- mkVty cfg
+     draw vty
