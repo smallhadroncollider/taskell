7bae0b6cc3de0e3d92d587eab2e572601489846e
Mark Wales <mark@smallhadroncollider.com>
2017-11-19 17:10:12 +0000

    updated current state to store in tuple

--- src/Flow/State.hs
+++ src/Flow/State.hs
@@ -4,47 +4,57 @@ import Data.Taskell.Task (Tasks, completed, empty)
 import Data.Sequence (mapWithIndex) 
 
 data State = State {
-    tasks :: Tasks,
-    current :: Int,
-    running :: Bool,
-    list :: Int
+    running :: Bool, -- whether the app is running
+    tasks :: Tasks, -- the tasks
+    current :: (Int, Int) -- the list and index
 } deriving (Show)
 
 initial :: State
 initial = (State {
         tasks = empty,
-        current = 0,
+        current = (0, 0),
         running = True
     }) 
 
-setCurrent :: State -> Int -> State
-setCurrent s i = s { current = i }
+-- list and index
+setIndex :: State -> Int -> State
+setIndex s i = s { current = (getList s, i) }
 
-count :: State -> Int
-count = length . tasks
+setList :: State -> Int -> State
+setList s l = s { current = (l, getIndex s) }
 
-index :: Int -> State -> State
-index i s = s { current = x }
+getIndex :: State -> Int
+getIndex s = snd $ current s
+
+getList :: State -> Int
+getList s = fst $ current s
+
+shiftIndex :: Int -> State -> State
+shiftIndex i s = setIndex s x 
     where
-        inc = ((current s) + i)
+        inc = (getIndex s) + i
         x = inc `mod` count s
 
 next :: State -> State
-next = index 1 
+next = shiftIndex 1 
 
 previous :: State -> State
-previous = index (-1)
+previous = shiftIndex (-1)
 
 switch :: State -> State
-switch s = s { list = l }
-    where l = if list s == 1 then 0 else 1
+switch s = setList s l
+    where l = if getList s == 1 then 0 else 1
+
+-- length
+count :: State -> Int
+count = length . tasks
 
 -- not terribly efficient
 -- goes over ever item
 mapCompleted :: State -> Tasks
 mapCompleted s = mapWithIndex set (tasks s)
     where
-        i = current s
+        i = getIndex s
         set = \cur t -> if cur == i then t { completed = (not (completed t)) } else t
 
 setCompleted :: State -> State
