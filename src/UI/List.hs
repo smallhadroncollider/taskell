3b4594909bf6bb9c1b8827ac68ac55ffe5b283ee
Mark Wales <mark@smallhadroncollider.com>
2017-11-19 16:41:59 +0000

    added missing type declarations

--- src/UI/List.hs
+++ src/UI/List.hs
@@ -5,11 +5,14 @@ import Data.Foldable (toList)
 import Graphics.Vty
 
 import UI.Task (present)
+import Data.Taskell.Task (Tasks)
 
 attrTitle :: Attr
 attrTitle = defAttr `withForeColor` green
 
+title :: String -> Image
 title t = string attrTitle t
 
+list :: (Tasks -> Tasks) -> String -> Tasks -> Image
 list fn name tasks = (title name) <-> items
     where items = vertCat $ toList $ mapWithIndex present $ fn $ tasks
--- src/UI/Main.hs
+++ src/UI/Main.hs
@@ -15,6 +15,7 @@ marginLeft :: Image -> Image
 marginLeft = pad 15 0 0 0
 
 -- creates the title element
+title :: Image
 title = marginBottom $ string attrTitle "[Taskell]"
 
 -- draws the screen
