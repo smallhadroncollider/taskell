d0f9556cf963eaf309726ddc1b7ef54f5915ec8e
Mark Wales <mark@smallhadroncollider.com>
2017-11-24 22:09:20 +0000

    pressing enter whilst adding new task creates new one

--- src/Flow/Actions/Insert.hs
+++ src/Flow/Actions/Insert.hs
@@ -4,7 +4,7 @@ import Graphics.Vty.Input.Events
 import Flow.State
 
 insert :: Event -> Stateful
-insert (EvKey KEnter _) = finishInsert
+insert (EvKey KEnter _) = newItem
 insert (EvKey KEsc _) = finishInsert
 insert (EvKey KBS _) = insertBS
 insert (EvKey (KChar char) _) = insertCurrent char
