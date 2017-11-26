54db28f3404b52e51333e041ee2b3a9718816e62
Mark Wales <mark@smallhadroncollider.com>
2017-11-26 17:33:02 +0000

    brought back horizontal scrolling

--- src/UI/Main.hs
+++ src/UI/Main.hs
@@ -3,19 +3,34 @@ module UI.Main where
 import Graphics.Vty
 import Data.Foldable (toList)
 
-import Data.Sequence (Seq, (<|), mapWithIndex)
+import Prelude hiding (take)
+import Data.Sequence (Seq, mapWithIndex, take)
 
-import Flow.State (State, lists, current)
+import Flow.State (State, lists, current, size)
 
 import UI.Styles
 
-import Config (width)
+import Config (width, padding)
 import Data.Taskell.String (wrap)
 import Data.Taskell.Task (description)
 import Data.Taskell.List (List, tasks, title)
 
 present :: List -> ([String], Seq [String])
 present l = (wrap width (title l), wrap width . description <$> tasks l)
+
+-- draws the screen
+pic :: State -> Picture
+pic s = Picture (Cursor x' 0) [translateX offset $ hCat lists'] ClearBackground
+    where ls = present <$> lists s
+          (l, i) = current s
+          r' = render (l, i)
+          parse i (t, ts) = margin $ renderTitle (i == l) t <-> (vCat . mapWithIndex (\j -> marginTop . r' (i, j))) ts
+          lists' = mapWithIndex parse ls
+          x = padding + sum (imageWidth <$> take l lists')
+          (w, h) = size s
+          pivot = w `div` 3
+          offset = if x > pivot then pivot - x else 0
+          x' = x + offset
 
 -- styling
 task :: [String] -> Image
@@ -24,19 +39,11 @@ task = img attrNormal
 currentTask :: [String] -> Image
 currentTask = img attrCurrent
 
-renderTitle :: [String] -> Image
-renderTitle = marginTop . img attrTitle
+renderTitle :: Bool -> [String] -> Image
+renderTitle current = marginTop . img (if current then attrCurrent else attrTitle)
 
 render :: (Int, Int) -> (Int, Int) -> ([String] -> Image)
 render a b = if a == b then currentTask else task
-
--- draws the screen
-pic :: State -> Picture
-pic s = Picture NoCursor [image] ClearBackground
-    where ls = present <$> lists s
-          r' = render (current s)
-          parse i (t, tasks) = margin $ renderTitle t <-> (vCat . mapWithIndex (\j -> marginTop . r' (i, j))) tasks
-          image = hCat $ mapWithIndex parse ls
 
 -- vty helpers
 img :: Attr -> [String] -> Image
