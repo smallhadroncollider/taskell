3962b9f496bc604bb762ac28d6a715a25e546f46
Mark Wales <mark@smallhadroncollider.com>
2017-11-29 14:01:28 +0000

    stopped Maybe propagation from breaking everything

--- src/UI/Main.hs
+++ src/UI/Main.hs
@@ -25,27 +25,40 @@ present l = (wrap width (title l), wrap width . description <$> tasks l)
 rList :: ListUI -> Image
 rList (t, ts) = margin $ img attrTitle t <-> (vCat . fmap (marginTop . task)) ts
 
-rCurList :: Size -> Int -> ListUI -> Maybe (Image, Int, Int)
-rCurList (w, h) i (t, ts) = do
-    (a, c, b) <- splitOn i ts
-    let title = img attrCurrentTitle t
-    let r = vCat . fmap (marginTop . task)
-    let pre = r a
-    let cur = marginTop (currentTask c)
-    let y = imageHeight pre + imageHeight cur
-    let x = if not (null c) then length (last c) else 0
-    let o = calcOffset (h `div` 2) y
-    let img = margin $ title <-> pre <-> cur <-> r b
-    return (translateY o img, x, y + o)
+justTitle :: TaskUI -> Image
+justTitle = img attrCurrentTitle
 
-cur :: Pointer -> Size -> Seq ListUI -> Maybe (Image, Int, Int, Int)
-cur (l, i) s ls = do
+len :: TaskUI -> Int
+len = sum . fmap length
+
+curList :: Size -> Int -> TaskUI -> (Seq TaskUI, TaskUI, Seq TaskUI) -> (Image, Int, Int)
+curList (w, h) i t (a, c, b) = (translateY o img, x, y + o)
+    where title = justTitle t 
+          r = vCat . fmap (marginTop . task)
+          pre = r a
+          cur = marginTop (currentTask c)
+          y = imageHeight pre + imageHeight cur
+          x = if not (null c) then length (last c) else 0
+          o = calcOffset (h `div` 2) y
+          img = margin $ title <-> pre <-> cur <-> r b
+
+rCurList :: Size -> Int -> ListUI -> (Image, Int, Int)
+rCurList s i (t, ts) = case splitOn i ts of
+    Just l -> curList s i t l
+    Nothing -> (justTitle t, len t, 0)
+
+cur' :: Pointer -> Size -> Seq ListUI -> Maybe (Image, Int, Int, Int)
+cur' (l, i) s ls = do
     (a, c, b) <- splitOn l ls
     let r = hCat . fmap rList
     let pre = r a
-    (cur, x, y) <- rCurList s i c
+    let (cur, x, y) = rCurList s i c
     let img = pre <|> cur <|> r b
     return (img, imageWidth pre, x, y)
+
+cur :: Pointer -> Size -> Seq ListUI -> (Image, Int, Int, Int)
+cur p s ls = fromMaybe (string attrNormal "No lists", 0, 0, 0) c
+    where c = cur' p s ls
 
 calcOffset :: Int -> Int -> Int
 calcOffset pivot n = if n > pivot then pivot - n else 0
@@ -54,16 +67,12 @@ offset :: Size -> Int -> Int
 offset (w, _) = calcOffset (w `div` 3)
 
 -- draws the screen
-pic' :: State -> Maybe Picture
-pic' s = do
-    let ls = present <$> lists s
-    let sz = size s
-    (img, w, x, y) <- cur (current s) sz ls
-    let o = offset sz w
-    return $ Picture (Cursor (w + x + o + padding) (y + 1)) [translateX o $ marginTop img] ClearBackground
-
 pic :: State -> Picture
-pic s = fromMaybe (picForImage $ string attrError "Something's gone wrong...") (pic' s)
+pic s = Picture (Cursor (w + x + o + padding) (y + 1)) [translateX o $ marginTop img] ClearBackground
+    where ls = present <$> lists s
+          sz = size s
+          (img, w, x, y) = cur (current s) sz ls
+          o = offset sz w
 
 -- styling
 task :: TaskUI -> Image
