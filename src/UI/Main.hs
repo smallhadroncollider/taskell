4ec02bca949170bb8d144a74f5b66d1d5851fc24
Mark Wales <mark@smallhadroncollider.com>
2017-11-30 12:23:27 +0000

    tidied up UI.Main

--- src/UI/Main.hs
+++ src/UI/Main.hs
@@ -12,7 +12,7 @@ import UI.Styles
 
 import Config (width, padding)
 import Data.Taskell.String (wrap)
-import Data.Taskell.Seq (splitOn)
+import Data.Taskell.Seq (Split, splitOn)
 import Data.Taskell.Task (description)
 import Data.Taskell.List (List, tasks, title)
 
@@ -22,43 +22,47 @@ type ListUI = (TaskUI, Seq TaskUI)
 present :: List -> ListUI
 present l = (wrap width (title l), wrap width . description <$> tasks l)
 
-rList :: ListUI -> Image
-rList (t, ts) = margin $ img attrTitle t <-> (vCat . fmap (marginTop . task)) ts
+titleImage :: TaskUI -> Image
+titleImage = img attrCurrentTitle
 
-justTitle :: TaskUI -> Image
-justTitle = img attrCurrentTitle
+taskLength:: TaskUI -> Int
+taskLength= sum . fmap length
 
-len :: TaskUI -> Int
-len = sum . fmap length
+tasksImage :: Seq TaskUI -> Image
+tasksImage = vCat . fmap (marginTop . task)
 
-curList :: Size -> Int -> TaskUI -> (Seq TaskUI, TaskUI, Seq TaskUI) -> (Image, Int, Int)
-curList (w, h) i t (a, c, b) = (translateY o img, x, y + o)
-    where title = justTitle t 
-          r = vCat . fmap (marginTop . task)
-          pre = r a
-          cur = marginTop (currentTask c)
-          y = imageHeight pre + imageHeight cur
-          x = if not (null c) then length (last c) else 0
-          o = calcOffset (h `div` 2) y
-          img = margin $ title <-> pre <-> cur <-> r b
+renderCurrentList' :: Size -> Int -> TaskUI -> Split TaskUI -> (Image, Int, Int)
+renderCurrentList' (w, h) i t (before, current, after) = (translateY yOffset img, x, y + yOffset)
+    where title = titleImage t 
+          [before', after'] = tasksImage <$> [before, after] 
+          current' = marginTop (currentTask current)
+          y = sum $ imageHeight <$> [before', current']
+          x = if not (null current) then length (last current) else 0
+          yOffset = calcOffset (h `div` 2) y
+          img = margin $ vertCat [title, before', current', after']
 
-rCurList :: Size -> Int -> ListUI -> (Image, Int, Int)
-rCurList s i (t, ts) = case splitOn i ts of
-    Just l -> curList s i t l
-    Nothing -> (margin (justTitle t), len t, 0)
+renderCurrentList :: Size -> Int -> ListUI -> (Image, Int, Int)
+renderCurrentList s i (t, ts) = case splitOn i ts of
+    Just l -> renderCurrentList' s i t l
+    Nothing -> (margin (titleImage t), taskLength t, 0)
 
-cur' :: Pointer -> Size -> Seq ListUI -> Maybe (Image, Int, Int, Int)
-cur' (l, i) s ls = do
-    (a, c, b) <- splitOn l ls
-    let r = hCat . fmap rList
-    let pre = r a
-    let (cur, x, y) = rCurList s i c
-    let img = pre <|> cur <|> r b
-    return (img, imageWidth pre, x, y)
+listImage :: ListUI -> Image
+listImage (t, ts) = margin $ img attrTitle t <-> tasksImage ts
 
-cur :: Pointer -> Size -> Seq ListUI -> (Image, Int, Int, Int)
-cur p s ls = fromMaybe (string attrNormal "No lists", 0, 0, 0) c
-    where c = cur' p s ls
+listsImage :: Seq ListUI -> Image
+listsImage = hCat . fmap listImage
+
+renderLists' :: Pointer -> Size -> Seq ListUI -> Maybe (Image, Int, Int, Int)
+renderLists' (l, i) s ls = do
+    (before, current, after) <- splitOn l ls
+    let [before', after'] = listsImage <$> [before, after]
+    let (current', x, y) = renderCurrentList s i current
+    let img = horizCat [before', current', after']
+    return (img, imageWidth before', x, y)
+
+renderLists :: Pointer -> Size -> Seq ListUI -> (Image, Int, Int, Int)
+renderLists p s ls = fromMaybe (string attrNormal "No lists", 0, 0, 0) c
+    where c = renderLists' p s ls
 
 calcOffset :: Int -> Int -> Int
 calcOffset pivot n = if n > pivot then pivot - n else 0
@@ -72,7 +76,7 @@ pic s = Picture cursor [translateX o $ marginTop img] ClearBackground
     where s' = newList s
           ls = present <$> lists s' 
           sz = size s'
-          (img, w, x, y) = cur (current s') sz ls
+          (img, w, x, y) = renderLists (current s') sz ls
           o = offset sz w
           cursor = if showCursor s then Cursor (w + x + o + padding) (y + 1) else NoCursor
 
