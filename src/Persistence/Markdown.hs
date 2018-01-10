53956d02c28a3b59912dfd3e3f8c110a04457206
Mark Wales <mark@smallhadroncollider.com>
2018-01-10 18:13:48 +0000

    added Markdown stringify

--- src/Persistence/Markdown.hs
+++ src/Persistence/Markdown.hs
@@ -1,15 +1,18 @@
-module Persistence.Markdown (parse) where
+module Persistence.Markdown (
+    parse,
+    stringify
+) where
 
-import Prelude hiding (lines, null)
+import Prelude hiding (lines)
 
 import Data.Taskell.Lists (Lists, newList, appendToLast)
-import Data.Taskell.List (empty)
-import Data.Taskell.Task (Task, new)
+import Data.Taskell.List (List, empty, title, tasks)
+import Data.Taskell.Task (Task, new, description)
 import Data.Taskell.String (trim)
-import Data.List (foldl')
+import Data.Foldable (foldl')
 import Data.Sequence (Seq, fromList)
 import Data.ByteString.Lazy (ByteString)
-import Data.ByteString.Lazy.Char8 (lines, null, isPrefixOf, pack, unpack)
+import Data.ByteString.Lazy.Char8 (lines, isPrefixOf, pack, unpack)
 
 trimTitle :: ByteString -> String
 trimTitle s = trim . drop 2 $ unpack s
@@ -24,3 +27,12 @@ start ls s | pack "##" `isPrefixOf` s  = newList (trimTitle s) ls
 
 parse :: ByteString -> Lists
 parse s = foldl' start (fromList []) $ lines s
+
+taskToString :: String -> Task -> String
+taskToString s t = s ++ "- " ++ description t ++ "\n"
+
+listToString :: String -> List -> String
+listToString s l = s ++ (if null s then "" else "\n") ++ "## " ++ title l ++ "\n\n" ++ foldl' taskToString "" (tasks l)
+
+stringify :: Lists -> ByteString
+stringify ls = pack $ foldl' listToString "" ls
