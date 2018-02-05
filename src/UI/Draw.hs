571a97a57b2f254ec86e0457f6b25281e8a9a147
Mark Wales <mark@smallhadroncollider.com>
2018-02-05 16:38:36 +0000

    fix (UI.Draw): brought back column numbering

--- src/UI/Draw.hs
+++ src/UI/Draw.hs
@@ -6,7 +6,7 @@ module UI.Draw (
 
 import Flow.State (State, Mode(..), InsertMode(..), Pointer, lists, current, mode, search, newList)
 import Brick
-import Data.Text (Text, length)
+import Data.Text (Text, length, pack, concat)
 import Data.Taskell.List (List, tasks, title)
 import Data.Taskell.Task (Task, description)
 import Data.Taskell.Text (wrap)
@@ -42,9 +42,9 @@ renderTask p li ti t =
     where d = wrap width $ description t
           attr = if (li, ti) == p then taskCurrentAttr else taskAttr
 
--- columnNumber :: Int -> Text -> Text
--- columnNumber i s = if col >= 1 && col <= 9 then show col `Data.Text.append` ". " `Data.Text.append` s else s
---     where col = i + 1
+columnNumber :: Int -> Text -> Text
+columnNumber i s = if col >= 1 && col <= 9 then Data.Text.concat [pack (show col), ". ",  s] else s
+    where col = i + 1
 
 renderTitle :: Pointer -> Int -> List -> Widget ResourceName
 renderTitle (p, _) li l =
@@ -52,7 +52,7 @@ renderTitle (p, _) li l =
     . addCursor li (-1) d
     $ box d
 
-    where d = wrap width $ title l
+    where d = wrap width $ columnNumber li (title l)
           attr = if p == li then titleCurrentAttr else titleAttr
 
 renderList :: Pointer -> Int -> List -> Widget ResourceName
