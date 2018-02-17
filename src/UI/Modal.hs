1aed5888cae2394d5c51db3b8381e80f6b78a532
Mark Wales <mark@smallhadroncollider.com>
2018-02-17 13:46:35 +0000

    fix (UI.Modal): improved wrapping for Modals

--- src/UI/Modal.hs
+++ src/UI/Modal.hs
@@ -16,27 +16,35 @@ import Data.Taskell.Text (wrap)
 import Data.FileEmbed (embedFile)
 import Data.Foldable (toList)
 import Data.Sequence (mapWithIndex)
+import Data.Maybe (fromMaybe)
 import IO.Markdown (trimListItem)
 
 import UI.Types (ResourceName(..))
 import UI.Theme (titleAttr, taskCurrentAttr, disabledAttr)
 
-width :: Int
-width = 50
+maxWidth :: Int
+maxWidth = 50
 
-place :: Widget ResourceName -> Widget ResourceName
-place = padTopBottom 1 . centerLayer . border . padTopBottom 1 . padLeftRight 4 . hLimit width
-
-modal :: Text -> Widget ResourceName -> Widget ResourceName
-modal title w = place $ t <=> w'
-    where t = padLeftRight 2 . padBottom (Pad 1) . withAttr titleAttr $ txtWrap title
+modal :: Int -> (Text, Widget ResourceName) -> Widget ResourceName
+modal width (title, w) = t <=> w'
+    where t = padLeftRight 2 . padBottom (Pad 1) . withAttr titleAttr $ box (wrap width title)
           w'= viewport RNModal Vertical w
 
-help :: Widget ResourceName
-help = modal "Controls" w
+surround :: (Int -> (Text, Widget ResourceName)) -> Widget ResourceName
+surround fn = Widget Fixed Fixed $ do
+    ctx <- getContext
+    let fullWidth = availWidth ctx
+        padding = 4
+        w = (if fullWidth > maxWidth then maxWidth else fullWidth) - (padding * 2)
+        widget = modal (w - 4) $ fn w
+    render . padTopBottom 1 . centerLayer . border . padTopBottom 1 . padLeftRight padding . hLimit w $ widget
+
+
+help :: Int -> (Text, Widget ResourceName)
+help _ = ("Controls", w)
     where ls = T.lines $ decodeUtf8 $(embedFile "templates/controls.md")
           (l, r) = unzip $ breakOn ":" . T.replace "`" "" . trimListItem <$> ls
-          left = padRight (Pad 2) . vBox $ withAttr taskCurrentAttr . txt <$> l
+          left = padLeftRight 2 . vBox $ withAttr taskCurrentAttr . txt <$> l
           right = vBox $ txt . T.strip . T.drop 1 <$> r
           w = left <+> right
 
@@ -50,29 +58,30 @@ addCursor li d = showCursor n (Location (h, v))
 box :: [Text] -> Widget ResourceName
 box d = padBottom (Pad 1) . vBox $ txt <$> d
 
-renderSubTask :: Int -> Int -> SubTask -> Widget ResourceName
-renderSubTask current i subtask | i == current = visible $ withAttr taskCurrentAttr widget
+renderSubTask :: Int -> Int -> Int -> SubTask -> Widget ResourceName
+renderSubTask width current i subtask | i == current = visible $ withAttr taskCurrentAttr widget
                                 | complete subtask = withAttr disabledAttr widget
                                 | otherwise = widget
     where postfix = if complete subtask then " ✓" else ""
           text = wrap (width - 6) $ name subtask
           widget = txt "• " <+> addCursor i text (box text) <+> txt postfix
 
-st :: State -> Maybe (Widget ResourceName)
-st state = do
+st' :: State -> Int -> Maybe (Text, Widget ResourceName)
+st' state width = do
     task <- getCurrentTask state
     index <- getCurrentSubTask state
     let sts = subTasks task
         w | null sts = withAttr disabledAttr $ txt "  No sub-tasks"
-          | otherwise = vBox . toList $ renderSubTask index `mapWithIndex` sts
-    return $ modal (description task) w
+          | otherwise = vBox . toList $ renderSubTask width index `mapWithIndex` sts
+    return (description task, w)
+
+st :: State -> Int -> (Text, Widget ResourceName)
+st state width = fromMaybe ("Error", txt "Oops") $ st' state width
 
 getModal :: State -> ModalType -> [Widget ResourceName]
 getModal s t = case t of
-    Help -> [help]
-    SubTasks _ _ -> case st s of
-        Just w -> [w]
-        Nothing -> []
+    Help -> [surround help]
+    SubTasks _ _ -> [surround $ st s]
 
 showModal :: State -> [Widget ResourceName] -> [Widget ResourceName]
 showModal s view = case mode s of
