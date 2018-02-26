c26184304fc6eae9ad36fa95087537fab0cbcbb5
Mark Wales <mark@smallhadroncollider.com>
2018-02-26 15:28:22 +0000

    refactor (Data.Taskell.Text): refactored Data.Taskell.Text

--- src/Data/Taskell/Text.hs
+++ src/Data/Taskell/Text.hs
@@ -1,30 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Data.Taskell.Text (
     backspace,
     wrap
 ) where
 
-import Data.Text (Text, foldl', singleton, snoc, init, null, append, strip, empty)
+import Data.Text as T (Text, foldl', singleton, snoc, init, null, append, strip, empty)
 import Data.List (foldl')
 import Brick.Widgets.Core (textWidth)
 
 backspace :: Text -> Text
-backspace s = if not (Data.Text.null s) then Data.Text.init s else s
+backspace s = if not (T.null s) then T.init s else s
 
 -- wrapping
 spl' :: [Text] -> Char -> [Text]
 spl' ts c
     | c == ' ' = ts ++ [" "] ++ [empty]
-    | Prelude.null ts = [Data.Text.singleton c]
-    | otherwise = Prelude.init ts ++ [Data.Text.snoc (Prelude.last ts) c]
+    | Prelude.null ts = [T.singleton c]
+    | otherwise = Prelude.init ts ++ [T.snoc (Prelude.last ts) c]
 
 spl :: Text -> [Text]
-spl = Data.Text.foldl' spl' [empty]
+spl = T.foldl' spl' [empty]
 
 wrap :: Int -> Text -> [Text]
 wrap width = Data.List.foldl' (combine width) [empty] . spl
 
 combine :: Int -> [Text] -> Text -> [Text]
-combine width acc s = if nl then acc ++ [strip s] else Data.Taskell.Text.append (l `Data.Text.append` s) acc
+combine width acc s = if nl then acc ++ [strip s] else Data.Taskell.Text.append (l `T.append` s) acc
     where l = if Prelude.null acc then empty else last acc
           nl = textWidth l + textWidth s > width
 
