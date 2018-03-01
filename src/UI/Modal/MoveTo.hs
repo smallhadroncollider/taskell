16d73a1a7992d40b14f9a8246f58d6c1749f76cf
Mark Wales <mark@smallhadroncollider.com>
2018-03-01 14:44:21 +0000

    style (UI.Modal.MoveTo): updated MoveTo modal styling so letters are highlighted

--- src/UI/Modal/MoveTo.hs
+++ src/UI/Modal/MoveTo.hs
@@ -6,12 +6,13 @@ module UI.Modal.MoveTo (
 import Events.State (State, lists, getCurrentList)
 import Brick
 import Data.Taskell.List (title)
-import Data.Text as T (Text, concat, singleton)
+import Data.Text as T (Text, singleton)
 import Data.Sequence (deleteAt)
 import Data.Foldable (toList)
 
 import UI.Field (textField)
 import UI.Types (ResourceName)
+import UI.Theme (taskCurrentAttr)
 
 moveTo :: State -> (Text, Widget ResourceName)
 moveTo state = ("Move To:", widget)
@@ -19,7 +20,7 @@ moveTo state = ("Move To:", widget)
           ls = toList . deleteAt skip $ lists state
           titles = textField . title <$> ls
 
-          letter a = padRight (Pad 1) . txt $ T.concat ["[", singleton a, "]"]
+          letter a = padRight (Pad 1) . hBox $ [txt "[", withAttr taskCurrentAttr $ txt (singleton a), txt "]"]
           letters = letter <$> ['a'..]
 
           output (l, t) = l <+> t
