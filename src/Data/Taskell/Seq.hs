c0fdf74e98d7930ca1995274bd95c87ef9dcaa51
Mark Wales <mark@smallhadroncollider.com>
2018-04-04 11:36:05 +0100

    test (test/Data.Taskell.Seq): added tests for Data.Taskell.Seq

--- src/Data/Taskell/Seq.hs
+++ src/Data/Taskell/Seq.hs
@@ -12,8 +12,7 @@ extract i xs = do
     return (a, c)
 
 shiftBy :: Int -> Int -> Seq a  -> Maybe (Seq a)
-shiftBy from dir xs | from == 0 && dir < 0 = Nothing
-                    | otherwise = do
-                        current <- xs !? from
-                        let r = deleteAt from xs
-                        return $ insertAt (from + dir) current r
+shiftBy from dir xs = do
+    current <- xs !? from
+    let r = deleteAt from xs
+    return $ insertAt (from + dir) current r
