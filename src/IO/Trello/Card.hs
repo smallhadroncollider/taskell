46688fae52314e692b183f17a5bfce85eb11c8f3
Mark Wales <mark@smallhadroncollider.com>
2018-05-10 18:01:06 +0100

    feat (IO.Trello): added description field to Trello import

--- src/IO/Trello/Card.hs
+++ src/IO/Trello/Card.hs
@@ -9,11 +9,12 @@ module IO.Trello.Card (
 import ClassyPrelude
 
 import Data.Aeson
-import Data.Taskell.Task (Task, new)
+import Data.Taskell.Task (Task, new, setSummary)
 
 data Card = Card {
     name :: Text
+  , desc :: Text
 } deriving (Eq, Show, Generic, ToJSON, FromJSON)
 
 cardToTask :: Card -> Task
-cardToTask = new . name
+cardToTask card = setSummary (desc card)  $ new (name card)
