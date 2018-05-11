8cebe54757bf795c0c9e2f5277b12cf07a2f00f1
Mark Wales <mark@smallhadroncollider.com>
2018-05-11 08:57:27 +0100

    feat (UI.Modal.SubTasks): added summary to sub tasks view

--- src/UI/Modal/SubTasks.hs
+++ src/UI/Modal/SubTasks.hs
@@ -10,7 +10,7 @@ import Data.Sequence (mapWithIndex)
 
 import Brick
 
-import Data.Taskell.Task (SubTask, description, subTasks, name, complete)
+import Data.Taskell.Task (Task, SubTask, description, subTasks, name, complete, summary)
 import Events.State (State, getCurrentTask)
 import Events.State.Modal.SubTasks (getCurrentSubTask, getField)
 import UI.Field (Field, textField, widgetFromMaybe)
@@ -21,13 +21,19 @@ renderSubTask :: Maybe Field -> Int -> Int -> SubTask -> Widget ResourceName
 renderSubTask f current i subtask = padBottom (Pad 1) final
 
     where cur = i == current
-          postfix = if complete subtask then " ✓" else ""
-          text = name subtask ++ postfix
+          prefix = if complete subtask then "[x] " else "[ ] "
+          text = prefix ++ name subtask
           widget = textField text
           widget' = widgetFromMaybe widget f
           final | cur = visible $ withAttr taskCurrentAttr widget'
                 | complete subtask = withAttr disabledAttr widget
                 | otherwise = widget
+
+renderSummary :: Task -> Widget ResourceName
+renderSummary task = padTop (Pad 1) $ padBottom (Pad 2) w
+    where w = case summary task of
+            Just s -> txt s
+            Nothing -> txt "No description"
 
 st :: State -> (Text, Widget ResourceName)
 st state = fromMaybe ("Error", txt "Oops") $ do
@@ -39,4 +45,4 @@ st state = fromMaybe ("Error", txt "Oops") $ do
         w | null sts = withAttr disabledAttr $ txt "No sub-tasks"
           | otherwise = vBox . toList $ renderSubTask f i `mapWithIndex` sts
 
-    return (description task, w)
+    return (description task, renderSummary task <=> w)
