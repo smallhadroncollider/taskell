33217cd9179928afe64a8a347eff3509cd14ce66
Mark Wales <mark@smallhadroncollider.com>
2018-05-23 15:18:26 +0100

    docs (UI.Draw): added some comments

--- src/UI/Draw.hs
+++ src/UI/Draw.hs
@@ -24,6 +24,7 @@ import UI.Modal (showModal)
 import UI.Theme
 import UI.Types (ResourceName(..))
 
+-- | Draw needs to know various pieces of information, so keep track of them in a record
 data DrawState = DrawState {
     dsLists :: Lists
   , dsMode :: Mode
@@ -34,33 +35,38 @@ data DrawState = DrawState {
   , dsEditingTitle :: Bool
 }
 
+-- | Use a Reader to pass around DrawState
 type ReaderDrawState = ReaderT DrawState Identity
 
+-- | Takes a task's 'due' property and renders a date with appropriate styling (e.g. red if overdue)
 renderDate :: Maybe Day -> ReaderDrawState (Maybe (Widget ResourceName))
-renderDate day = do
-    today <- dsToday <$> ask
-    let attr = withAttr . dlToAttr . deadline today <$> day
-        widget = txt . dayToText today <$> day
+renderDate dueDay = do
+    today <- dsToday <$> ask -- get the value of `today` from DrawState
+    let attr = withAttr . dlToAttr . deadline today <$> dueDay -- create a `Maybe (Widget -> Widget)` attribute function
+        widget = txt . dayToText today <$> dueDay -- get the formatted due date `Maybe Text`
     return $ attr <*> widget
 
+-- | Renders the appropriate completed sub task count e.g. "[2/3]"
 renderSubTaskCount :: Task -> Widget ResourceName
-renderSubTaskCount task = str $ concat ["[" , show $ countCompleteSubTasks task , "/" , show $ countSubTasks task , "]"]
+renderSubTaskCount task = txt $ concat ["[" , tshow $ countCompleteSubTasks task , "/" , tshow $ countSubTasks task , "]"]
 
+-- | Renders the appropriate indicators: summary, sub task count, and due date
 indicators :: Task -> ReaderDrawState (Widget ResourceName)
 indicators task = do
-    dateWidget <- renderDate (due task)
+    dateWidget <- renderDate (due task) -- get the due date widget
     return . hBox $ padRight (Pad 1) <$> catMaybes [
-            const (txt "≡") <$> summary task
-          , bool Nothing (Just (renderSubTaskCount task)) (hasSubTasks task)
+            const (txt "≡") <$> summary task -- show the summary indicator if one is set
+          , bool Nothing (Just (renderSubTaskCount task)) (hasSubTasks task) -- if it has subtasks, render the sub task count
           , dateWidget
         ]
 
+-- | Renders an individual task
 renderTask :: Int -> Int -> Task -> ReaderDrawState (Widget ResourceName)
 renderTask listIndex taskIndex task = do
-    eTitle <- dsEditingTitle <$> ask
-    cur <- (== (listIndex, taskIndex)) . dsCurrent <$>ask
-    taskField <- dsField <$> ask
-    after <- indicators task
+    eTitle <- dsEditingTitle <$> ask -- is the title being edited? (for visibility)
+    selected <- (== (listIndex, taskIndex)) . dsCurrent <$>ask -- is the current task selected?
+    taskField <- dsField <$> ask -- get the field, if it's being edited
+    after <- indicators task -- get the indicators widget
 
     let text = description task
         name = RNTask (listIndex, taskIndex)
@@ -68,32 +74,35 @@ renderTask listIndex taskIndex task = do
         widget' = widgetFromMaybe widget taskField
 
     return $ cached name
-        . (if not eTitle && cur then visible else id)
+        . (if selected && not eTitle then visible else id)
         . padBottom (Pad 1)
         . (<=> withAttr disabledAttr after)
-        . withAttr (if cur then taskCurrentAttr else taskAttr)
-        $ if cur && not eTitle then widget' else widget
+        . withAttr (if selected then taskCurrentAttr else taskAttr)
+        $ if selected && not eTitle then widget' else widget
 
 
+-- | Gets the relevant column number for a list title
 columnNumber :: Int -> Text
 columnNumber i = if col >= 1 && col <= 9 then pack (show col) ++ ". " else ""
     where col = i + 1
 
+-- | Renders the title for a list
 renderTitle :: Int -> List -> ReaderDrawState (Widget ResourceName)
 renderTitle listIndex list = do
-    (p, i) <- dsCurrent <$> ask
-    cur <- (p == listIndex &&) . dsEditingTitle <$> ask
+    (selectedList, selectedTask) <- dsCurrent <$> ask
+    editing <- (selectedList == listIndex &&) . dsEditingTitle <$> ask
     titleField <- dsField <$> ask
 
     let text = title list
         col = txt $ columnNumber listIndex
-        attr = if p == listIndex then titleCurrentAttr else titleAttr
+        attr = if selectedList == listIndex then titleCurrentAttr else titleAttr
         widget = textField text
         widget' = widgetFromMaybe widget titleField
-        title' = padBottom (Pad 1) . withAttr attr . (col <+>) $ if cur then widget' else widget
+        title' = padBottom (Pad 1) . withAttr attr . (col <+>) $ if editing then widget' else widget
 
-    return $ if cur || p /= listIndex || i == 0 then visible title' else title'
+    return $ if editing || selectedList /= listIndex || selectedTask == 0 then visible title' else title'
 
+-- | Renders a list
 renderList :: Int -> List -> ReaderDrawState (Widget ResourceName)
 renderList listIndex list = do
     layout <- dsLayout <$> ask
@@ -112,8 +121,9 @@ renderList listIndex list = do
 
     return $ if currentList == listIndex then visible widget else widget
 
-searchImage :: Widget ResourceName -> ReaderDrawState (Widget ResourceName)
-searchImage mainWidget = do
+-- | Renders the search area
+renderSearch :: Widget ResourceName -> ReaderDrawState (Widget ResourceName)
+renderSearch mainWidget = do
     m <- dsMode <$> ask
     case m of
         Search editing searchField -> do
@@ -123,12 +133,13 @@ searchImage mainWidget = do
             return $ mainWidget <=> widget
         _ -> return mainWidget
 
+-- | Renders the main widget
 main :: ReaderDrawState (Widget ResourceName)
 main = do
     ls <- dsLists <$> ask
     listWidgets <- toList <$> sequence (renderList `mapWithIndex` ls)
     let mainWidget = viewport RNLists Horizontal . padTopBottom 1 $ hBox listWidgets
-    searchImage mainWidget
+    renderSearch mainWidget
 
 getField :: Mode -> Maybe Field
 getField (Insert _ _ f) = Just f
