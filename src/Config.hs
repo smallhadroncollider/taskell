9ebf6a38da8a6ee7f479c7293ea57c3da4a5acc6
Mark Wales <mark@smallhadroncollider.com>
2018-08-03 10:26:45 +0100

    chore (package.yaml): version bump

--- src/Config.hs
+++ src/Config.hs
@@ -8,7 +8,7 @@ import ClassyPrelude
 import Data.FileEmbed (embedFile)
 
 version :: Text
-version = "1.3.0"
+version = "1.3.1"
 
 usage :: Text
 usage = decodeUtf8 $(embedFile "templates/usage.txt")
