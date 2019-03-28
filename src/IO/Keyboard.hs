32a2eee08a9da5101ea0771e77390db14075e0f3
Mark Wales <mark@smallhadroncollider.com>
2019-03-28 23:17:55 +0000

    refactor: who needs bisequence when you've got bitraverse?

--- src/IO/Keyboard.hs
+++ src/IO/Keyboard.hs
@@ -6,10 +6,10 @@ module IO.Keyboard
 
 import ClassyPrelude
 
-import Data.Bitraversable (bisequence)
+import Data.Bitraversable (bitraverse)
 
 import IO.Keyboard.Types
 
 generate :: Bindings -> Actions -> BoundActions
 generate bindings actions =
-    mapFromList . catMaybes $ bisequence <$> (bimap bindingToEvent (`lookup` actions) <$> bindings)
+    mapFromList . catMaybes $ bitraverse bindingToEvent (`lookup` actions) <$> bindings
