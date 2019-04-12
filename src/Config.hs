7ca9c800f24b1b4f045a1c25018a50f4a902cb9e
Mark Wales <mark@smallhadroncollider.com>
2019-04-12 20:46:40 +0100

    chore: version bump

--- src/Config.hs
+++ src/Config.hs
@@ -9,7 +9,7 @@ import ClassyPrelude
 import Data.FileEmbed (embedFile)
 
 version :: Text
-version = "1.4.1"
+version = "1.4.2"
 
 usage :: Text
 usage = decodeUtf8 $(embedFile "templates/usage.txt")
