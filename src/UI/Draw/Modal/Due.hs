5d249e2690eb1ab3a723ada6dd4604d4e2e91838
Mark Wales <mark@smallhadroncollider.com>
2019-08-15 17:29:42 +0100

    feat: adds grouping by date

--- src/UI/Draw/Modal/Due.hs
+++ src/UI/Draw/Modal/Due.hs
@@ -8,16 +8,20 @@ module UI.Draw.Modal.Due
 import ClassyPrelude
 
 import Brick
+import Control.Lens ((^.))
+
 import Data.Taskell.Seq ((<#>))
 
-import qualified Data.Taskell.Task as T (Task)
+import qualified Data.Taskell.Task as T (Task, due)
 import           Types             (Pointer)
 import           UI.Draw.Task      (TaskWidget (..), parts)
-import           UI.Draw.Types     (DSWidget, ModalWidget)
+import           UI.Draw.Types     (ModalWidget, ReaderDrawState, TWidget)
 import           UI.Theme          (taskAttr, taskCurrentAttr)
 import           UI.Types          (ResourceName (RNDue))
 
-renderTask :: Int -> Int -> T.Task -> DSWidget
+type DueItem = (Maybe Day, TWidget, TWidget)
+
+renderTask :: Int -> Int -> T.Task -> ReaderDrawState DueItem
 renderTask current position task = do
     (TaskWidget text date _ _) <- parts task
     let selected = current == position
@@ -29,9 +33,23 @@ renderTask current position task = do
             if selected
                 then visible
                 else id
-    pure . shw . cached (RNDue position) . padBottom (Pad 1) . withAttr attr $ vBox [date, text]
+    let tWidget = shw . cached (RNDue position) . padBottom (Pad 1) . withAttr attr $ text
+    pure (task ^. T.due, date, tWidget)
+
+renderList :: [(Maybe Day, TWidget)] -> DueItem -> [(Maybe Day, TWidget)]
+renderList acc (day, date, text) =
+    case lastMay lst of
+        Just (prevDay, widget) ->
+            if prevDay == day
+                then rst <> [(day, widget <=> text)]
+                else acc <> new
+        Nothing -> new
+  where
+    (rst, lst) = splitAt (length acc - 1) acc
+    new = [(day, padTop (Pad 1) date <=> text)]
 
 due :: Seq (Pointer, T.Task) -> Int -> ModalWidget
 due tasks selected = do
     widgets <- sequence $ renderTask selected <#> (snd <$> tasks)
-    pure ("Due Tasks", vBox $ toList widgets)
+    let list = foldl' renderList empty widgets
+    pure ("Due Tasks", vBox $ snd <$> list)
